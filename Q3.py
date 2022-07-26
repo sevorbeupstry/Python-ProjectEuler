f08f6d43194ec2f2c2b9ba937588e7774813293d
Tugba Buyukbese <Tugba@Kylo-Ren.local>
2022-07-26 21:35:29 +0100

    add what the question is in each file

--- Q3.py
+++ Q3.py
@@ -1,3 +1,6 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# What is the largest prime factor of the number 600851475143 ?
+
 def factors(n):
     listoffactors = []
     for i in range(1,10000):
@@ -5,12 +8,16 @@ def factors(n):
             listoffactors.append(i)
     return listoffactors
 
+# FIRST ATTEMPT
+# 
 # def primefactors(n):
 #     listofprimefactors = []
 #     for factor in factors(n):
 #         if len(factors(factor)) == 2:
 #             listofprimefactors.append(factor)
 #     return listofprimefactors
+#
+#  BETTER VERSION BELOW
 
 def isPrime(n):
     return factors(n)==[1,n]
